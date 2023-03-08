1733e3b75f4a5cb40af56c72e33ad96b2df3177f
Pedro Candido <pedrohcn02@gmail.com>
2023-03-08 09:36:45 -0300

    button registering teams done

--- backend/src/teams.routes.js
+++ backend/src/teams.routes.js
@@ -7,15 +7,21 @@ const teamsRoutes = express.Router();
 const prisma = new PrismaClient();
 
 teamsRoutes.post("/register", async function (req, res) {
-	const { name } = req.body;
+	const { nome } = req.body;
 
 	const team = await prisma.team.create({
 		data: {
-			nome: name,
+			nome,
 		}
 	});
 
 	return res.status(201).json(team);
+});
+
+teamsRoutes.get("/teams", async function (req, res) {
+	const allTeams = await prisma.team.findMany();
+
+	return res.status(200).json(allTeams);
 });
 
 module.exports = teamsRoutes;
